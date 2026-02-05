9885eb722e6aad15ad161da021a3522153f8ae07
anwarhussen36 <anwarhussen3683@gmail.com>
2026-02-05 19:06:37 +0300

    Fix: Remove unused adapter-pg imports causing build failure

--- backend-ecommerce/src/prisma/prisma.service.ts
+++ backend-ecommerce/src/prisma/prisma.service.ts
@@ -1,7 +1,5 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
-import { Pool } from 'pg';
-import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
